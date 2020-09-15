4f3fdcb9ebc3ddf0be949a81e0bad8135b9c09ae
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-15 21:27:56 +0530

    Initialized Project

---
+++ Movieflex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movieflex
+//
+//  Created by Shubham Singh on 15/09/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
