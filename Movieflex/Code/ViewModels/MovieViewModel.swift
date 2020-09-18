d0d5332a133331b19df2d851d3011d6db8e83cf6
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-18 13:31:00 +0530

    MovieViewModels Updated

--- Movieflex/Code/ViewModels/MovieViewModel.swift
+++ Movieflex/Code/ViewModels/MovieViewModel.swift
@@ -10,6 +10,9 @@ import UIKit
 
 struct MovieViewModel {
     
+    let fileHandler: FileHandler
+    let networkManager: NetworkManager
+    
     let titleInfo: TitleInfo
     let id: String
     let rating: TitleRating
@@ -33,6 +36,8 @@ struct MovieViewModel {
         return url
     }
     
+    var moviePosterImage: BoxBind<UIImage?> = BoxBind(nil)
+    
     var movieTitle: String {
         titleInfo.title
     }
@@ -41,7 +46,7 @@ struct MovieViewModel {
         genres.joined(separator: ", ")
     }
     
-    init(meta: TitleMetaData) {
+    init(meta: TitleMetaData, handler: FileHandler, networkManager: NetworkManager) {
         self.titleInfo = meta.title
         self.id = meta.titleId
         self.rating = meta.ratings
@@ -52,6 +57,23 @@ struct MovieViewModel {
         } else {
             self.releaseDate = ""
         }
+        
+        self.fileHandler = handler
+        self.networkManager = networkManager
+        
+        self.getMoviePoster()
+    }
+    
+    func getMoviePoster() {
+        if (fileHandler.checkIfFileExists(titleId: id)) {
+            self.moviePosterImage.value = UIImage(contentsOfFile: fileHandler.getPathForImage(titleId: id).path)
+        } else {
+            networkManager.downloadMoviePoster(url: self.moviePosterUrl, titleId: self.id) { res, error in
+                if (error == .none) {
+                    self.moviePosterImage.value = UIImage(contentsOfFile: fileHandler.getPathForImage(titleId: id).path)
+                }
+            }
+        }
     }
 }
 
@@ -61,27 +83,47 @@ struct MovieListViewModel {
     // MARK:- variable for the viewModel
     let defaultsManager: UserDefaultsManager
     let networkManager: NetworkManager
+    let fileHandler: FileHandler
     
     var offset: Int = 0
     var limit: Int = 10
     
     var popularMovies: BoxBind<[MovieViewModel]?> = BoxBind(nil)
+    var comingSoonMovies: BoxBind<[MovieViewModel]?> = BoxBind(nil)
+    
     
     // MARK:- initializers for the viewModel
-    init(defaultsManager: UserDefaultsManager, networkManager: NetworkManager) {
+    init(defaultsManager: UserDefaultsManager, networkManager: NetworkManager, handler: FileHandler) {
         self.defaultsManager = defaultsManager 
         self.networkManager = networkManager
+        self.fileHandler = handler
         
         self.getPopularMovieTitles(offset: offset, limit: limit)
+        self.getComingSoonTitles(offset: offset, limit: limit)
     }
     
     // MARK:- functions for the viewModel
     func getPopularMovieTitles(offset: Int, limit: Int) {
         let moviesList = defaultsManager.getPopularTitlesList()
-        let titleIds = moviesList[offset..<limit]
-        networkManager.getTitlesMetaData(titleIds: Array(titleIds)) { res, error in
-            guard let titlesMetaData = res else { return }
-            self.popularMovies.value = titlesMetaData.map { MovieViewModel(meta: $0)}
+        if (moviesList.isEmpty) {
+        } else {
+            let titleIds = moviesList[offset..<limit]
+            networkManager.getTitlesMetaData(titleIds: Array(titleIds)) { res, error in
+                guard let titlesMetaData = res else { return }
+                self.popularMovies.value = titlesMetaData.map { MovieViewModel(meta: $0, handler: self.fileHandler, networkManager: networkManager)}
+            }
+        }
+    }
+    
+    func getComingSoonTitles(offset: Int, limit: Int) {
+        let moviesList = defaultsManager.getComingSoonTitlesList()
+        if (moviesList.isEmpty) {
+        } else {
+            let titleIds = moviesList[offset..<limit]
+            networkManager.getTitlesMetaData(titleIds: Array(titleIds)) { res, error in
+                guard let titlesMetaData = res else { return }
+                self.comingSoonMovies.value = titlesMetaData.map { MovieViewModel(meta: $0, handler: self.fileHandler, networkManager: networkManager)}
+            }
         }
     }
 }
---
+++ Movieflex/Extensions/CoreGraphicsExtension.swift
@@ -0,0 +1,9 @@
+//
+//  CoreGraphicsExtension.swift
+//  Movieflex
+//
+//  Created by Shubham Singh on 18/09/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import Foundation
