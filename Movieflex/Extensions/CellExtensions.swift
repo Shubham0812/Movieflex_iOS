e1d3a558916096fcc9627e0a11ea22741e231d39
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-22 13:57:00 +0530

    Extensions modified

--- Movieflex/Extensions/CellExtensions.swift
+++ Movieflex/Extensions/CellExtensions.swift
@@ -14,8 +14,19 @@ extension UICollectionViewCell {
     }
 }
 
-extension UICollectionReusableView {
-    func aNib() -> UINib {
+extension UITableViewCell {
+    func asNib() -> UINib {
         return UINib(nibName: Self.description(), bundle: nil)
+    }
+}
+
+extension UICollectionView {
+    func getSizeForHorizontalCollectionView(columns: CGFloat, height: CGFloat) -> CGSize {
+        let collectionViewWidth = self.bounds.width
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        
+        let adjustedWidth = collectionViewWidth - (flowLayout.minimumLineSpacing * (columns - 1))
+        let width = floor(adjustedWidth / columns)
+        return CGSize(width: width, height: height)
     }
 }
--- Movieflex/Extensions/FoundationExtensions.swift
+++ Movieflex/Extensions/FoundationExtensions.swift
@@ -23,3 +23,11 @@ extension Sequence where Iterator.Element: Hashable {
         return filter { seen.insert($0).inserted }
     }
 }
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
