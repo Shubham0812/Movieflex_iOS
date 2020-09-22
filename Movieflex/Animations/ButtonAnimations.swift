2d76862c64d01513c62a9a828c9ce6596cc57ee6
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-22 13:56:44 +0530

    Animations Modified

--- Movieflex/Animations/ButtonAnimations.swift
+++ Movieflex/Animations/ButtonAnimations.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 Shubham Singh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+typealias ButtonConfiguration = (symbol: String, configuration:  UIImage.SymbolConfiguration, buttonTint: UIColor)
+
+struct ButtonAnimationFactory {
+    
+    func makeActivateAnimation(for button: UIButton, _ config: ButtonConfiguration) {
+        button.layer.opacity = 0.5
+        button.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
+            button.layer.opacity = 1
+            button.transform = CGAffineTransform(scaleX: 1, y: 1)
+            button.setImage(UIImage(systemName: config.symbol, withConfiguration: config.configuration), for: .normal)
+            button.tintColor = config.buttonTint
+        })
+    }
+    
+    func makeDeactivateAnimation(for button: UIButton, _ config: ButtonConfiguration) {
+        button.layer.opacity = 0.5
+        button.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
+            button.layer.opacity = 1
+            button.transform = CGAffineTransform(scaleX: 1, y: 1)
+            button.setImage(UIImage(systemName: config.symbol, withConfiguration: config.configuration), for: .normal)
+            button.tintColor = config.buttonTint
+        })
+    }
+}
--- Movieflex/Animations/UIViewAnimations.swift
+++ Movieflex/Animations/UIViewAnimations.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 Shubham Singh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ViewAnimationFactory {
+    static func makeEaseInAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseIn, animations: {
+            action()
+        })
+    }
+    static func makeEaseInOutAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseInOut, animations: {
+            action()
+        })
+    }
+    static func makeEaseOutAnimation(duration: TimeInterval, delay: TimeInterval, action: @escaping() -> Void) {
+        UIView.animate(withDuration: duration, delay: delay, options: .curveEaseOut, animations: {
+            action()
+        })
+    }
+    
+    static func makeSimpleAnimation(duration: TimeInterval, action: @escaping() -> Void){
+        UIView.animate(withDuration: duration) {
+            action()
+        }
+    }
+}
